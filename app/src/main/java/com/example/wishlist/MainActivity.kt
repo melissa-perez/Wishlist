720a1955a760cc1561787667a94aee6d597543f2
Melissa Perez <melmperz@gmail.com>
2023-03-04 01:00:15 -0500

    Add empty array

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.wishlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val wishes: List<Wish> = listOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val wishesRV: RecyclerView = findViewById(R.id.wishList)
+        val wishAdapter = WishItemAdapter(wishes)
+        wishesRV.adapter = wishAdapter
+        wishesRV.layoutManager = LinearLayoutManager(this)
     }
 }
